93b42be5b7aa1d9bb6251fb7b1a6008a6b837bd6
rufus159 <foxsfire@163.com>
2025-05-17 11:40:03 +0800

    Update diy-feeds.sh

--- diy-feeds.sh
+++ diy-feeds.sh
@@ -20,9 +20,7 @@
 # 安装包
 ./scripts/feeds install -a 
 
-echo >> feeds.conf.default
-echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
-
-./scripts/feeds update qmodem
-
-./scripts/feeds install -a -f -p qmodem
+#echo >> feeds.conf.default
+#echo 'src-git qmodem https://github.com/FUjr/QModem.git;main' >> feeds.conf.default
+#./scripts/feeds update qmodem
+#./scripts/feeds install -a -f -p qmodem
